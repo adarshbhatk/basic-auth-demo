8afbfbcf157f6bbabefb0eb35e7a031d640dad00
Adarsh K <adarshbhatk@gmail.com>
2024-07-20 21:59:54 +0530

    Salt and hash password

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import dotenv from "dotenv";
+import bcrypt from "bcryptjs";
 
 dotenv.config();
+
+const saltRounds = 10;
 
 const app = express();
 const port = 3000;
@@ -37,36 +40,61 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
   try {
-    const email = req.body.username;
-    const password = req.body.password;
-    const existingUser = await db.query("SELECT email FROM users WHERE email = $1", [email]);
-    if(existingUser.rows.length > 0) {
-      res.send("Email already exists!");
+    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    if (checkResult.rows.length > 0) {
+      res.send("Email already exists! Try logging in.");
+    } else {
+      //Hashing the password before saving it in the database
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) {
+          console.error("Error hashing password: ", err);
+        } else {
+          console.log("Hashed Password: ", hash);
+          await db.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2)",
+            [email, hash]
+          );
+          res.render("secrets.ejs");
+        }
+      });
     }
-    else {
-      const result1 = await db.query("INSERT INTO users (email, password) VALUES ($1, $2)", [email, password]);
-      console.log(result1);
-      res.render("secrets.ejs");
-    }
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    console.log(err);
   }
 });
 
 app.post("/login", async (req, res) => {
+  const email = req.body.username;
+  const loginPassword = req.body.password;
+
   try {
-    const email = req.body.username;
-    const password = req.body.password;
-    const result2 = await db.query("SELECT password FROM users WHERE email = $1", [email]);
-    const userPassword = result2.rows[0];
-    if(userPassword.password === password) {
-      res.render("secrets.ejs");
+    const result = await db.query("SELECT * FROM users WHERE email = $1", [email]);
+    if (result.rows.length > 0) {
+      const user = result.rows[0];
+      const storedHashedPassword = user.password;
+      //Verifying the password
+      bcrypt.compare(loginPassword, storedHashedPassword, (err, result) => {
+        if (err) {
+          console.error("Error comparing passwords: ", err);
+        } else {
+          if (result) {
+            res.render("secrets.ejs");
+          } else {
+            res.send("Incorrect password!");
+          }
+        }
+      });
     } else {
-      res.send("Wrong password! Try again.");
+      res.send("User not found!");
     }
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    console.log(err);
   }
 });
 
